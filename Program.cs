2ad3802e82173a42677b673792a276590fd3f4f0
Tony <akruk@kse.org.ua>
2023-02-20 22:53:14 +0200

    Fixed program interface

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
 ﻿using lab4_huffman;
 
-string text = File.ReadAllText("../../../sherlock.txt");
+var text = File.ReadAllText("../../../sherlock.txt");
 
-// Print char and its frequency in the Dictionary
-
-Tree tree = new Tree(text);
-Dictionary<char, string> codes = tree.GetBinaryCodes();
+var tree = new Tree(text);
+var codes = tree.GetBinaryCodes();
 
 Console.WriteLine("Initial bits: " + text.Length * 8);
 
 Console.WriteLine("Binary codes:");
-foreach (KeyValuePair<char, string> pair in codes) {
-    Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+foreach (var pair in codes) {
+    Console.WriteLine($"{pair.Key}: {pair.Value}");
 }
